352ce89a3452ae78698a1976b0aa2f1bbaee89bb
Justin Greger <justingreger@gmail.com>
2022-12-18 12:40:46 -0500

    completed.

---
+++ arrays/exercise_3.rb
@@ -0,0 +1,8 @@
+# How do you return the word "example" from the following array?
+
+arr = [["test", "hello", "world"], ["example", "mem"]]
+
+# Either of the following solutions will work:
+
+p arr.last.first
+p arr[1][0]
