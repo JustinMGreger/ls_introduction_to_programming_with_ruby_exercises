f4c7222cc009d2115ddea5737e479ca61b6d7e10
Justin Greger <justingreger@gmail.com>
2022-12-18 18:15:53 -0500

    completed.

---
+++ exercises/exercise_7.rb
@@ -0,0 +1,16 @@
+=begin
+
+What's the major difference between an Array and a Hash?
+
+Solution:
+
+The major difference between an array and a hash is that a hash contains a key value pair for referencing by key.
+
+=end
+
+hash = { dog: 'fido', cat: 'fluffy' }
+
+array = ['fido', 'fluffy']
+
+puts hash[:dog]
+puts array[0]
