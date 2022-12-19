82d4661f5e18db941cfa38c2b9f441976cf2415f
Justin Greger <justingreger@gmail.com>
2022-12-18 19:42:20 -0500

    exercise_4.rb

---
+++ methods/exercise_4.rb
@@ -0,0 +1,30 @@
+=begin
+What will the following code print to the screen?
+
+def scream(words)
+  words = words + "!!!!"
+  return  #explicit return causes ruby to exit from the method. Subsequent code will not be executed.
+  puts words #this line will never be executed as a result.
+end
+
+scream("Yippeee") #"Yippeee!!!!"
+
+#solution: It will not print anything to the screen.
+
+1) Edit the method definition in exercise #4 so that it does print words on the screen. 2) What does it return now?
+
+1. def scream(words)
+      words = words + "!!!!"
+      puts words #removed the explicit return.
+    end
+
+    scream("Yippeee")
+
+ 2. still returns nil
+=end
+
+def scream(words)
+  words = words + "!!!!"
+  puts words #output of nil
+end
+p scream("Yippeee") #output of "Yippeee!!!!"
