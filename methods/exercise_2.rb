474d13b21c707b7c3b4a4d8aab7a67da009cfea6
Justin Greger <justingreger@gmail.com>
2022-12-18 19:39:31 -0500

    completed.

---
+++ methods/exercise_2.rb
@@ -0,0 +1,14 @@
+=begin
+What do the following expressions evaluate to? That is, what value does each expression return?
+1. x = 2
+2. puts x = 2
+3. p name = "Joe"
+4. four = "four"
+5. print something = "nothing"
+Solution
+1. x = 2    # => 2
+2. puts x = 2    # => nil
+3. p name = "Joe"    # => "Joe"
+4. four = "four"    # => "four"
+5. print something = "nothing"    # => nil
+=end
