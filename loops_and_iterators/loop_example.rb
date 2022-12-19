85fba0f8e7d75657601a21b135cf033a24ba6c16
Justin Greger <justingreger@gmail.com>
2022-12-18 19:24:43 -0500

    completed.

---
+++ loops_and_iterators/loop_example.rb
@@ -0,0 +1,22 @@
+# Let's try an example of a loop by creating a file named loop_example.rb
+
+# loop_example.rb
+
+loop do
+  puts "This will keep printing until you hit Ctrl + c"
+end
+
+# Now we can run ruby loop_example.rb on the terminal and see what happens.
+
+# You'll notice the same statement keeps printing on the terminal. You'll have to interrupt with a Ctrl + c to stop it.
+=begin
+
+This will keep printing until you hit Ctrl + c
+This will keep printing until you hit Ctrl + c
+This will keep printing until you hit Ctrl + c
+This will keep printing until you hit Ctrl + c
+This will keep printing until you hit Ctrl + cInterrupt:
+from (pry):2:in `puts'
+[2] pry(main)>
+
+=end
