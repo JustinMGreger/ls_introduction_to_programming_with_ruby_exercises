60b482f818deda6a83f52e76e7ad6f81211a5f70
Justin Greger <justingreger@gmail.com>
2022-12-18 19:12:59 -0500

    completed.

--- loops_and_iterators/countdown3.rb
+++ loops_and_iterators/countdown3.rb
@@ -1,5 +1,4 @@
 # In Ruby, for loops are used to loop over a collection of elements. Unlike a while loop, where if we're not careful we can cause an infinite loop, a for loop has a definite end since it's looping over a finite number of elements. It begins with the for reserved word, followed by a variable, then the in reserved word, and then a collection of elements. We'll show this using an array and a range. A range is a special type in Ruby that captures a range of elements. For example 1..3 is a range that captures the integers 1, 2, and 3.
-
 # countdown3.rb
 
 x = gets.chomp.to_i
