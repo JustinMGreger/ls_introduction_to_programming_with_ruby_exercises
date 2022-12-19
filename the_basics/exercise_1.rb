58021fcaf771a6d24188ff4771703cacc8e0c0d1
Justin Greger <justingreger@gmail.com>
2022-12-18 20:59:29 -0500

    completed.

---
+++ the_basics/exercise_1.rb
@@ -0,0 +1,6 @@
+#Add two strings together that, when concatenated, return your first and last name as your full name in one string.
+first_name = "first_name"
+last_name = "last_name"
+puts first_name + last_name #string concatenation without a space.
+puts first_name + " " + last_name #string contatenation with a space.
+puts "#{first_name} #{last_name}" #string interpolation.
