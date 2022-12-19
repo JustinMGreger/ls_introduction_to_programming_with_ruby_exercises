d19c3bc6e64facf7a6a95e40628fc8a66e2184b8
Justin Greger <justingreger@gmail.com>
2022-12-18 20:55:56 -0500

    proc_example.rb

---
+++ more_stuff/proc_example.rb
@@ -0,0 +1,23 @@
+=begin
+
+Procs are blocks that are wrapped in a proc object (we'll cover objects in our object oriented programming book) and stored in a variable to be passed around. This is how you define a proc.
+
+# proc_example.rb
+
+talk = Proc.new do
+  puts "I am talking."
+end
+
+talk.call
+
+Procs can also take arguments if specified.
+
+=end
+
+# proc_example.rb
+
+talk = Proc.new do |name|
+  puts "I am talking to #{name}"
+end
+
+talk.call "Bob"
